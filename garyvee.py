7295289d5bbca063fc2577a754037cc635656c51
Dustin <dustinroberts96@gmail.com>
2020-03-21 18:49:43 -0400

    find new hashes

--- garyvee.py
+++ garyvee.py
@@ -6,8 +6,8 @@ import time
 import sqlite3
 import datetime
 import re
+import fnmatch
 
-th= []
 class GaryVee:
     conn = sqlite3.connect('instagram2.db')
 
@@ -17,9 +17,9 @@ class GaryVee:
     username = 'grizzlytreeservice'
     password = 'Cheerup22!'
     links = []
-    hashtags = ['elkhart']
+    hashtags = []
  
-
+    visited = []
     comments = [
         'Your posts are amazing', 'Amazing work. Keep going!', 'Your photos are magnificent',
         'Your work fascinates me!', 'I like how you put your posts together', 'Great job',
@@ -28,13 +28,28 @@ class GaryVee:
 
 
     price = 0.0
+    
+
 
     def __init__(self):
         self.browser = webdriver.Firefox(executable_path="./geckodriver")
-        #self.check()
-
+        self.loadHash()
+        self.loadVisited()
         self.login()
         self.hustle()
+
+    def loadHash(self):
+        g = self.conn.execute("SELECT hash from hashes")
+        for gg in g:
+            self.hashtags.append(str(gg[0]))
+        print(self.hashtags)
+
+    def loadVisited(self):
+        g = self.conn.execute("SELECT url from vistedlinks")
+        for ccc in g:
+            self.visited.append(ccc[0])
+        print("##########################################")
+        print(self.visited)
 
     def login(self):
         self.browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
@@ -56,13 +71,12 @@ class GaryVee:
 
     def hustle(self):
         self.getTopPosts()
-        #self.getNewHash()
         self.execute()
         self.finalize()
 
     def getTopPosts(self):
         for hashtag in self.hashtags:
-            self.browser.get('https://www.instagram.com/explore/tags/' + hashtag +'/')
+            self.browser.get('https://www.instagram.com/explore/tags' + hashtag)
             time.sleep(5)
 
             links = self.browser.find_elements_by_tag_name('a')
@@ -72,16 +86,13 @@ class GaryVee:
             try:
                  for i in range(0, 9):
                     link = valid_links[i].get_attribute('href')
-                    g = self.conn.execute("SELECT url from vistedlinks")
-                    for ccc in g:
-
-                       
-                        if str(ccc[0]) == str(link):
-                            print("*************already liked post************")
-                            pass
-                        else:
-                            if link not in self.links:
-                                self.links.append(link)
+                
+                 
+                    if link not in self.links:
+                        print("adding")
+                        self.links.append(link)
+                    else:
+                        print("Already commented on Post")
                        
             except:
                 pass
@@ -91,11 +102,11 @@ class GaryVee:
             self.browser.get(link)
             
             time.sleep(1)
-        
+            
 
             self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
-            
+            self.getNewHash()
             #self.comment()
             time.sleep(2)
             
@@ -122,29 +133,48 @@ class GaryVee:
     def like(self):
         like_button = lambda: self.browser.find_element_by_class_name('wpO6b ')
         like_button().click()
-    def post_server(self):
-        pass
+ 
+
+    def getNewHash(self):
+        pattern_list = ['elkhart', 'goshen', 'granger', 'southbend', 'mishawka', 'indiana', 'saintjoe', 'michiana']
+        #new_hash = self.browser.find_elements_by_tag_name('a')
+        hashes = self.browser.find_elements_by_class_name('xil3i')
+
+        condition = lambda link: '.com/explore/' in link.get_attribute('href')
+        valid_hash = list(filter(condition, hashes))
+        #th.append(valid_links)
+        
+        for i in range(len(valid_hash)-1):
+            hashlink = valid_hash[i].get_attribute('href')
+
+            for pattern in pattern_list:
+                if pattern in hashlink:
+                    s = hashlink
+                    start = s.find('tags/') + 4
+                    sss = s[start::]
+                    print(sss)
+                    if sss not in self.hashtags:
+                        self.c.execute('INSERT INTO hashes VALUES (?,?)', [(sss),("none")])
+                        self.conn.commit()
+                        print("added new hashag: ", hashlink)
+                    else:
+                        pass
+                else:
+                    pass
     def finalize(self):
         self.browser.close()
         print("finished sucessfully")
         for llinks in self.links:
             self.c.execute('INSERT INTO vistedlinks VALUES (?,?)', [('2006-03-28'),(llinks)])
             self.conn.commit()
+         hashtags = ['elkhart', 'elkahrtindiana', 'elkhartcounty', 'elkhartcounty4hfair', 'elkhartcountyparks', 
+                    'downtownelkhart', 'elkhartartwalk', 'elkhartcountyhistory', 'elkharteats', 'elkhartphotographystudio']
+
+        print(self.links)
         self.conn.close()
-
-
         sys.exit()
 
-    '''def getNewHash(self):
-        keywords = ['elkhart', 'goshen', 'granger', 'southbend', 'mishawka', 'indiana', 'saintjoe']
-        #new_hash = self.browser.find_elements_by_tag_name('a')
-        hashes = self.browser.find_elements_by_class_name('xil3i')
-        condition = lambda link: 'explore' in link.get_attribute('href')
-        valid_links = list(filter(condition, links))
-        th.append(valid_links)
-        c = conn.execute("SELECT date, trans, symbol from users")
-        for cc in c:
-            if cc ==
-            '''
+
 if __name__ == '__main__':
     garyVee = GaryVee()
+
